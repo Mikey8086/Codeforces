c791b94d02937064fe98d4e6fbbad882d24930f4
Mikey <amn2003ktr@gmail.com>
2026-08-19 21:54:51 +0530

    AC: 664504F - F - Rectangle

---
+++ Codeforces/664504F - F - Rectangle/solution.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(void){
+    
+    int a,b;
+    scanf("%d %d",&a,&b);
+    
+    printf("Area = %d\n",a*b);
+    printf("Perimeter = %d\n",2*(a+b));
+    
+   
+    return 0;
+}
