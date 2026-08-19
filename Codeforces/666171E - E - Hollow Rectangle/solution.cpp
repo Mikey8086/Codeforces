7c866bc2b4c2f3fdc086f55ac62a9a58f0fc2ce1
Mikey <amn2003ktr@gmail.com>
2026-08-20 01:12:25 +0530

    AC: 666171E - E - Hollow Rectangle

---
+++ Codeforces/666171E - E - Hollow Rectangle/solution.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+
+  int m, n;
+  scanf("%d %d", &m, &n);
+
+  for (int i = 0; i < m; i++) {
+    for (int j = 0; j < n; j++) {
+      if (i == 0 || j == 0 || i == m - 1 || j == n - 1) {
+        printf("*");
+      } else {
+        printf(" ");
+      }
+    }
+    printf("\n");
+  }
+}
