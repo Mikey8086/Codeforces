9a581d87f2aa41672bc18152af993820dd1c5d96
Mikey <amn2003ktr@gmail.com>
2026-08-19 21:54:45 +0530

    AC: 664504J - J - Even or Odd

---
+++ Codeforces/664504J - J - Even or Odd/solution.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(void){
+   
+   long long n;
+   scanf("%lld",&n);
+   if(n%2 == 0){
+       printf("Even\n");
+   }else{
+       printf("Odd\n");
+   }
+    
+    return 0;
+}
