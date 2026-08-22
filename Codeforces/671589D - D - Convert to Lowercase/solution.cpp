950a97e2759251c5a624cf714ebfeab7effc72ae
Mikey <amn2003ktr@gmail.com>
2026-08-23 04:05:32 +0530

    AC: 671589D - D - Convert to Lowercase

---
+++ Codeforces/671589D - D - Convert to Lowercase/solution.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void){
+
+  char ch;
+  scanf("%c",&ch);
+
+  if(ch>='A' && ch<='Z'){
+    printf("%c",ch+32);
+  }else{
+    printf("%c",ch);
+  }
+
+  return 0;
+}
+
