4c3e21fa9b84d692c6ce6c7cf431d3da769e2946
Mikey <amn2003ktr@gmail.com>
2026-08-22 19:14:10 +0530

    AC: 671594H - H - Wave - I

---
+++ Codeforces/671594H - H - Wave - I/solution.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void) {
+
+  int m, n;
+  scanf("%d %d", &m, &n);
+
+  int arr[m][n];
+
+  for (int i = 0; i < m; i++) {
+    for (int j = 0; j < n; j++) {
+      scanf("%d", &arr[i][j]);
+    }
+  }
+
+  for (int i = 0; i < m; i++) {
+    if (i % 2 == 0) {
+      for (int j = 0; j < n; j++) {
+        printf("%d ", arr[i][j]);
+      }
+    } else {
+      for (int j = n - 1; j >= 0; j--) {
+        printf("%d ", arr[i][j]);
+      }
+    }
+  }
+
+  return 0;
+}
