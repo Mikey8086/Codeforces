2e6247c8ae3880d3c2c68c2814fc180a5c67bd4a
Mikey <amn2003ktr@gmail.com>
2026-08-22 18:52:56 +0530

    AC: 671594F - F - Column Sum

---
+++ Codeforces/671594F - F - Column Sum/solution.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(void){
+
+  int m,n;
+  scanf("%d %d",&m,&n);
+
+  int arr[m][n];
+
+  for(int i=0; i<m; i++){
+    for(int j=0; j<n; j++){
+      scanf("%d",&arr[i][j]);
+    }
+  }
+
+  for(int i=0; i<n; i++){
+    int sum = 0;
+    for(int j=0; j<m; j++){
+      sum = sum + arr[j][i];
+    }
+    printf("%d ",sum);
+  }
+
+  return 0;
+
+}
